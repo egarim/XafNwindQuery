a153a00dfdfdfc6ddfbc2face179507b512adb65
Jose Ojeda <joche.ojeda@bitframeworks.com>
2023-03-11 13:45:05 +0300

    Add project files.

---
+++ XafNwindQuery.Module/BusinessObjects/NwindModelCode/SalesTotalsByAmount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+namespace XafNwindQuery.Module.BusinessObjects.Northwind
+{
+
+    public partial class SalesTotalsByAmount
+    {
+        public SalesTotalsByAmount(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
